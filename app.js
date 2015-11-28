059aa2981b27788dfefc5c51d74ab89358c62efe
Morten Brun <Morten Brun>
2015-11-29 00:47:39 +0100

    Forgot to save

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var mongoose = require ("mongoose");
 var stormpath = require('express-stormpath');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var competition = require('./routes/competition');
 
 var app = express();
 
@@ -19,6 +19,16 @@ app.use(stormpath.init(app, {
   application:  process.env.STORMPATH_URL,
   website:      true,
 }));
+
+var uristring = process.env.MONGOLAB_URI;
+    
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,7 +43,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/competition', competition);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
